aff762fb58f7616017e1174c0d7fadceaa487612
mr.zherdev1992@gmail.com <mr.zherdev1992@gmail.com>
2019-12-27 00:09:11 +0300

    1 1 HW0 stream

---
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+public class MealTo {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+//    private final Supplier<Boolean> excess;
+//    private final AtomicBoolean excess;
+    private final boolean excess;
+
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+
+//    public Boolean getExcess() {
+//        return excess.get();
+//    }
+
+    @Override
+    public String toString() {
+        return "MealTo{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", excess=" + excess +
+                '}';
+    }
+}
