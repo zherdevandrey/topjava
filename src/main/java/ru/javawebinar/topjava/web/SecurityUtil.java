2529cc95a8e0829d78c2447edfa70fc634d6f12b
mr.zherdev1992@gmail.com <mr.zherdev1992@gmail.com>
2019-12-30 22:34:16 +0300

    before Junit

---
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.web;
+
+import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+
+public class SecurityUtil {
+
+    private static int id = 1;
+
+    public static int authUserId() {
+        return id;
+    }
+
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
+    }
+
+    public static int authUserCaloriesPerDay() {
+        return DEFAULT_CALORIES_PER_DAY;
+    }
+}
